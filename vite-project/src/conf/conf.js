28549543fefa96adac7e5958712c91ef2ef33dff
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2024-10-19 19:40:31 +0545

    Commit

--- vite-project/src/conf/conf.js
+++ vite-project/src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwriteURL:String(import.meta .env.VITE_APPWRITE_URL),
-    appwriteProjectID:String(import.meta .env.VITE_PROJECT_ID),
-    appwriteDatabaseID:String(import.meta .env.VITE_DATABASE_ID),
-    appwriteCollectionID:String(import.meta .env.VITE_COLLECTION_ID),
-    appwriteBucketID:String(import.meta .env.VITE_STRING_ID)
+    appwriteURL:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID:String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseID:String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionID:String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketID:String(import.meta.env.VITE_STRING_ID),
 }
 
 export default conf;
